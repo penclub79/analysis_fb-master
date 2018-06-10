63d1986eb1ac5d70a30ba5ef5262110dee492886
penclub79 <penclub79@naver.com>
2018-06-10 17:56:09 +0900

    good

--- practice01/quiz_06.py
+++ practice01/quiz_06.py
@@ -1,7 +1,11 @@
 data = [1, 3, 5, 8, 9, 11, 15, 19, 18, 20, 30, 33, 31]
+s = 0
+sum = 0
 
 for data in data:
     if(data % 3 == 0):
-        coun
-        print(data)
+        s += 1
+        sum += data
+print('3의 배수 개수 : ', s)
+print('3의 배수 합 : ', sum)
 
